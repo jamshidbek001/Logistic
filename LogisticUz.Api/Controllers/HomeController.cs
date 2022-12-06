e7415ff6c6c39c25e4f9984109a1d89eddfeb079
jamshidbek001 <inomiddinovjamshidbek@gmail.com>
2022-12-06 18:55:26 +0100

    INFRA: Initialize API Project

---
+++ LogisticUz.Api/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿// =================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use Comfort And Peace
+// =================================================
+
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace LogisticUz.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class HomeController : RESTFulController
+    {
+        [HttpGet]
+        public ActionResult<string> Get() =>
+            Ok("Hello Mario, the princess is in another castle.");
+    }
+}
