fbeebae585fcc605c6c6e961cab54c14b78f36e2
jamshidbek001 <inomiddinovjamshidbek@gmail.com>
2022-12-07 10:34:55 +0100

    DATA: Model & Migrate Guests

---
+++ LogisticUz.Api/Brokers/Storages/StorageBroker.Guests.cs
@@ -0,0 +1,15 @@
+﻿// =================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use Comfort And Peace
+// =================================================
+
+using LogisticUz.Api.Controllers.Foundations.Customers;
+using Microsoft.EntityFrameworkCore;
+
+namespace LogisticUz.Api.Brokers.Storages
+{
+    public partial class StorageBroker
+    {
+        public DbSet<Guest> Guests { get; set; }
+    }
+}
