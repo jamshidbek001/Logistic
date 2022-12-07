716cc70d087d3f47f8a6327e752281d7cf90b31f
jamshidbek001 <inomiddinovjamshidbek@gmail.com>
2022-12-07 10:58:28 +0100

    BROKERS: Insert Guest

---
+++ LogisticUz.Api/Brokers/Storages/IStorageBroker.Guests.cs
@@ -0,0 +1,15 @@
+﻿// =================================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use Comfort And Peace
+// =================================================
+
+using System.Threading.Tasks;
+using LogisticUz.Api.Controllers.Foundations.Customers;
+
+namespace LogisticUz.Api.Brokers.Storages
+{
+    public partial interface IStorageBroker
+    {
+        ValueTask<Guest> InsertGuestAsync(Guest guest);
+    }
+}
